f88de3aea2f516b108dccb4840ad5f5962d87301
Harold2015 <harolddang2015@gmail.com>
2025-11-22 14:37:58 +1100

    I'm An Idiot!!!!!!!!!!!!!!!!!!!!!!!!:)

--- static/script.js
+++ static/script.js
@@ -1,12 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Button 1 changes text
-  const button1 = document.getElementById("button1");
-  if (button1) {
-    button1.addEventListener("click", () => {
-      const textElem = document.getElementById("changingtext1");
-      if (textElem) textElem.textContent = "You clicked the first button!!";
-    });
-  }
+
 
   // Button 2 changes text and image
   const button2 = document.getElementById("button2");
@@ -16,7 +8,27 @@ document.addEventListener("DOMContentLoaded", () => {
       if (textElem) textElem.textContent = "You clicked the second button!!";
 
       const imgElem = document.getElementById("changeimagebutton2");
-      if (imgElem) imgElem.src = "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_16x9.jpg?w=1200"; // replace with your new image path
+      if (imgElem) imgElem.src = "https://ibb.co/hFJcNPjm"; // replace with your new image path
+	  
+	  const textElem = document.getElementById("buh");
+      if (textElem) textElem.textContent = "so that is my face.";
     });
   }
 });
+ 
+ document.addEventListener("DOMContentLoaded", () => {
+  // Button 1 changes text
+  const button1 = document.getElementById("button1");
+  if (button1) {
+    button1.addEventListener("click", () => {
+      const textElem = document.getElementById("changingtext1");
+      if (textElem) textElem.textContent = "You clicked the first button!!";
+	  
+	  const textElem = document.getElementById("buh");
+      if (textElem) textElem.textContent = "hi";
+	  
+	  
+	  const imgElem = document.getElementById("changeimagebutton2");
+      if (imgElem) imgElem.src = "https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg"; // replace with your new image path
+    });
+  }
